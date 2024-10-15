fdbb06814432e894d7f6aafd6351ffbfa17b56b2
ProdJohn <fwtx.john@gmail.com>
2024-10-15 10:54:16 -0500

    morning push

---
+++ src/main/java/com/pluralsight/Transactions.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+public class Transactions {
+    private String date;
+    private String time;
+    private String vendor;
+    private String description;
+    private double amount;
+
+    public Transactions(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.time = vendor;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transactions{" +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
+
